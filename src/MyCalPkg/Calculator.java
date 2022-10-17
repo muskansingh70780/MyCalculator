c3c6536f717f16b58823f80fd3d3d38f8b38e1e8
Muskan <abc.gmail.com>
2022-10-17 11:33:51 +0530

    my first commit

---
+++ src/MyCalPkg/Calculator.java
@@ -0,0 +1,5 @@
+package MyCalPkg;
+
+public class Calculator {
+
+}
---
+++ src/MyCalPkg/MainCalculator.java
@@ -0,0 +1,10 @@
+package MyCalPkg;
+
+public class MainCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
